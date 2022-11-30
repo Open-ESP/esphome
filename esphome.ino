e77138eaa26defbcde10a2ff983d72e29faf07ff
r4venz <zexceed12300@gmail.com>
2022-11-30 21:51:50 +0700

    POST Temperature from esp to server
    Signed-off-by: r4venz <zexceed12300@gmail.com>

--- esphome.ino
+++ esphome.ino
@@ -4,11 +4,14 @@
 #include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 
-#define STA_WIFI_SSID               "Oppo"
+#define STA_WIFI_SSID               "ESPHOME"
 #define STA_WIFI_PASS               "12345678"
 
 const char* r4venz_host =           "esp.r4venz.me";
 const uint16_t r4venz_port =        8443;
+
+WiFiClientSecure client;
+HTTPClient https;
 
 void set_gpio_state(uint8_t pin, int state) {
     if (state == 0) {
@@ -21,45 +24,38 @@ void set_gpio_state(uint8_t pin, int state) {
     }
 }
 
-String request() {
-
-    WiFiClientSecure client;
-    HTTPClient https;
-
-    String payload = "{}";
-
-    client.setInsecure();
-
-    if (!client.connect(r4venz_host, r4venz_port)) {
-        Serial.println("Google Asisstant connection failed!..");
-    } else {
-        Serial.println("Google Asisstant connected!");
-    
-        https.begin(client, "http://esp.r4venz.me:8443/api/get_state");
-
-        if (https.GET() > 0) {
-            payload = https.getString();
-        }
-    }
-
-    return payload;
-}
-
 void handleAPI() {
-    if (WiFi.status()== WL_CONNECTED) {
+    if (WiFi.status() == WL_CONNECTED) {
 
-        String deviceState = request();
-        
-        DynamicJsonDocument json(1024);
-        deserializeJson(json, deviceState);
+        if (!client.connect(r4venz_host, r4venz_port)) {
 
-        Serial.println(deviceState);
+            Serial.println("Connected To GoogleHome");
 
-        set_gpio_state(4, json["1"]["on"]);
-        set_gpio_state(5, json["2"]["on"]);
-        set_gpio_state(12, json["3"]["on"]);
-        set_gpio_state(13, json["4"]["on"]);
-        set_gpio_state(14, json["5"]["on"]);
+            https.begin(client, "http://esp.r4venz.me:8443/api/get_state");
+            if (https.GET() > 0) {
+                String deviceState = https.getString();
+            
+                DynamicJsonDocument json(1024);
+                deserializeJson(json, deviceState);
+
+                Serial.println(deviceState);
+
+                set_gpio_state(4, json["1"]["on"]);
+                set_gpio_state(5, json["2"]["on"]);
+                set_gpio_state(12, json["3"]["on"]);
+                set_gpio_state(13, json["4"]["on"]);
+                set_gpio_state(14, json["5"]["on"]);
+            }
+            https.end();
+            
+            https.begin(client, "http://esp.r4venz.me:8443/api/set_state");
+            https.addHeader("Content-Type", "application/json");
+            https.POST("{\"6\":{\"online\":true,\"status\":\"SUCCESS\",\"thermostatMode\":\"cool\",\"thermostatTemperatureAmbient\":" + String(30) + ",\"thermostatTemperatureSetpoint\":" + String(30) + "}}");
+
+            https.end();
+        } else {
+            Serial.println("Connecting To GoogleHome Failed!");
+        }
     }
 }
 
@@ -72,6 +68,15 @@ void setup() {
     WiFi.mode(WIFI_STA);
 
     WiFi.begin(STA_WIFI_SSID, STA_WIFI_PASS);
+
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println();
+
+    client.setInsecure();
 
     pinMode(2, OUTPUT);
     pinMode(4, OUTPUT);
